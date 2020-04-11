f4c473a4a906469f3ae9a59088ea24e5e40c12c3
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-11 17:26:45 +0700

    done

---
+++ Demo/src/demoMang.java
@@ -0,0 +1,13 @@
+public class demoMang {
+    public static void main(String[] args) {
+//        double[] myList = new double[10];
+//        myList[0] = 13.6;
+//        myList[3] = 26.2;
+//        for (double item: myList) {
+//        System.out.println(item);
+//        }
+        int[] a = {1,2,3,4,5,6,7,8,9}, b = new int[3];
+        a[9] = b[0];
+        System.out.println(a[9]);
+    }
+}
